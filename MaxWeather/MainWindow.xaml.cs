1629e19ab7cb857b50b5a7e3f20fed9ab24c3a37
ㅤㅤㅤㅤㅤ MAX <mn_-@outlook.com>
2024-03-05 14:54:53 +0300

    Chart

--- MaxWeather/MainWindow.xaml.cs
+++ MaxWeather/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace MaxWeather
 {
@@ -21,12 +25,62 @@ namespace MaxWeather
     public partial class MainWindow : Window
     {
 
+        public SeriesCollection SeriesCollection { get; set; }
+        
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
         private List<string> cities = new List<string> { "Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург", "Казань","Кострома","Киев","Кейптаун","Керчь","Калининград" }; 
         public MainWindow()
         {
             InitializeComponent();
             citySearchTextBox.Text = "Поиск города";
             today.Content = "Сегодня " + $"{DateTime.Now:f}";
+            var temps = new ChartValues<int> { -12, -8, -7, -5, -5, -6, -8, -10, -13 };
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title="Температура",
+                    Values = temps,
+                    PointGeometrySize= 20,
+
+                    Stroke = (Brush)(new BrushConverter().ConvertFrom("#B7FF5E")),
+                    StrokeThickness = 3,
+                    Fill = Brushes.Transparent
+                    
+
+
+
+        },
+            };
+
+            weather.AxisX = new LiveCharts.Wpf.AxesCollection()
+
+{
+    new LiveCharts.Wpf.Axis()
+    {
+        Title= "Время",
+        Separator = new LiveCharts.Wpf.Separator()
+        {
+            Step = 1.0,
+            IsEnabled = false
+
+        },
+        FontSize=15
+        
+
+    }
+};         
+            Labels = new[] { "0:00", "3:00", "6:00", "9:00", "12:00", "15:00", "18:00", "21:00", "24:00" };
+            weather.AxisX.First().Labels = Labels;
+           
+
+            YFormatter = value => value + "°";
+            DataContext = this;
+
+          
+
 
         }
         private void SuggestionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
